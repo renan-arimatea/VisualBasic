9b487f98320ff6c946a7a892d42151e92d575875
Renan Arimatea <renanmoraiz@gmail.com>
2022-04-12 23:32:21 -0300

    Inicio do Projeto Sistema Clinica

---
+++ SistemaClinicaVeterinaria/SistemaClinicaVeterinaria/Conexao.vb
@@ -0,0 +1,22 @@
+﻿Imports MySqlConnector
+
+Module Conexao
+    Public mysql As New MySqlConnection("server=localhost;userid=root;password=;database=sistema_clinica")
+
+    Sub abrir()
+        If mysql.State = 0 Then
+            mysql.Open()
+            MsgBox("Conectado com sucesso!")
+
+        End If
+    End Sub
+
+    Sub fechar()
+        If mysql.State = 1 Then
+            mysql.Close()
+            MsgBox("Conexão fehada!")
+
+        End If
+    End Sub
+
+End Module
---
+++ SistemaClinicaVeterinaria/SistemaClinicaVeterinaria/Login.vb
@@ -0,0 +1,6 @@
+﻿Public Class Login
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        abrir()
+        fechar()
+    End Sub
+End Class
