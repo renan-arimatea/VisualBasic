132e24d509a18364926a07f99c214c9c9c753d1a
Renan Arimatea <renanmoraiz@gmail.com>
2022-04-16 21:25:32 -0300

    Criando sistema de média de notas de alunos.

---
+++ Notas/Notas/Program.vb
@@ -0,0 +1,43 @@
+Imports System
+
+Module Program
+
+    Private nota1 As Double
+    Private nota2 As Double
+    Private nota3 As Double
+    Private nota4 As Double
+
+    Sub Main()
+        Console.WriteLine("Informe a Nota 01: ")
+        nota1 = Console.ReadLine
+
+        Console.WriteLine("Informe a Nota 02: ")
+        nota2 = Console.ReadLine
+
+        Console.WriteLine("Informe a Nota 03: ")
+        nota3 = Console.ReadLine
+
+        Console.WriteLine("Informe a Nota 04: ")
+        nota4 = Console.ReadLine
+
+        Dim media As Double = (nota1 + nota2 + nota3 + nota4) / 4
+
+        If media >= 6 Then
+            Console.WriteLine("Aluno Aprovado com média: " & media)
+        Else
+            Console.WriteLine("Aluno Reprovado com média: " & media)
+        End If
+
+
+        Select Case media
+
+            Case >= 6
+                Console.WriteLine("Parabéns, você pode curtir as suas merecidas férias!")
+
+            Case Else
+                Console.WriteLine("Nos vemos novamente ano que vem!")
+        End Select
+
+        Console.ReadLine()
+    End Sub
+End Module
